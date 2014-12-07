7d0cf0ff74c372661a2c478a29d8b70e59871a0c
gpoisoned <gpoisoned@gmail.com>
2014-12-06 23:09:25 -0700

    Adds starter flask webserver app

---
+++ server/webserver.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return jsonify(status="Web server is running!")
+
+@app.route("/health")
+def health():
+    return jsonify(heath="It's all good :)")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='0.0.0.0', port=8080)
